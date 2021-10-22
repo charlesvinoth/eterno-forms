439c43b148b10b8beeef9d2356cbc38c0e0545c8
Charles Vinoth <charlezvinoth@gmail.com>
2021-10-22 22:36:05 +0530

    chore(other): bootstrap project

---
+++ src/quasar.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+
+import "./styles/quasar.scss";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/material-icons-outlined/material-icons-outlined.css";
+import "@quasar/extras/mdi-v4/mdi-v4.css";
+import "@quasar/extras/mdi-v5/mdi-v5.css";
+import "@quasar/extras/eva-icons/eva-icons.css";
+import { Quasar } from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  plugins: {},
+});
---
+++ src/views/Forms.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Forms",
+};
+</script>
+
+<style lang="scss" scoped></style>
